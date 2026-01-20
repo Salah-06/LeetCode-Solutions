5e8731a18034b9d18439899123fe6a72044c4b40
saud-cc <saud@correctcloud.io>
2026-01-21 00:37:02 +0530

    update code

--- TwoSum.py
+++ TwoSum.py
@@ -4,9 +4,9 @@ class Solution:
         for i, num in enumerate(nums):
             comp = target - num
             if comp in map:
-                print([map[comp], i])
                 return [map[comp], i]
             map[num] = i
 
 a = Solution()
-a.twoSum([2,7,11,15], 9)
+b = a.twoSum([2,7,11,15], 9)
+print(b)
